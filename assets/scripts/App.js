18b384980225504cb7df80724e25a9283fb0c1f6
jong-hui <webd200@gmail.com>
2020-01-12 08:40:58 +0900

    당신 어째서 em하지 않은거지

---
+++ assets/scripts/App.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+
+  document.querySelector('.mobile-menu').addEventListener('click', function () {
+    document.querySelector('body').classList.toggle('isSide')
+  })
+}
